e8c4ba106e4f9f2eba6259a31f26b1a8169dbe5d
Adam Käck <adam.kack@gmail.com>
2021-09-08 11:25:39 +0200

    WIP-MOB

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import React, { useState } from "react";
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [movieList, setMovieList] = useState([]);
   const API_URL = "http://www.omdbapi.com/?apikey=65049324&s="; // s=star+wars
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     console.log(inputText);
+    const data = await fetch(`${API_URL}${inputText}`);
+    console.log(data);
+    const response = await data.json();
+
+    // console.log(response);
+    if (response && response.Response === 'True') {
+        setMovieList(response.Search);
+    }
+
   };
 
   return (
