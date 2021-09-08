e65c1229ba25e864c86ffa8c140cc9f394e1d124
Jack Enqvist <jack.enqvist@tele2.com>
2021-09-08 11:13:39 +0200

    WIP-MOB

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const API_URL = "http://www.omdbapi.com/?apikey=65049324&s=";
+  const [inputText, setInputText] = useState("");
+  const API_URL = "http://www.omdbapi.com/?apikey=65049324&s="; // s=star+wars
 
-  return <h1>Hello Adam!</h1>;
+  const handleSearch = (e) => {
+    console.log(inputText);
+  };
+
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+        value={inputText}
+        type="text"
+        placeholder="movie title"
+      />
+      <button onClick={handleSearch}>Sök</button>
+      {inputText}
+    </div>
+  );
 }
 
 export default App;
