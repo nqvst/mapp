35ae187bae027faea531a72d0d50b0acb1690938
Jack Enqvist <jack.enqvist@tele2.com>
2021-09-08 09:54:27 +0200

    add the create react app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  const API_URL = "http://www.omdbapi.com/?apikey=65049324&s=";
+
+  return <h1>Hello!</h1>;
+}
+
+export default App;
