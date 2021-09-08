71e80095181d11636a23fba64956986cb2f2423e
Jack Enqvist <jack.enqvist@tele2.com>
2021-09-08 10:44:32 +0200

    WIP-MOB

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 function App() {
   const API_URL = "http://www.omdbapi.com/?apikey=65049324&s=";
 
-  return <h1>Hello!</h1>;
+  return <h1>Hello Adam!</h1>;
 }
 
 export default App;
