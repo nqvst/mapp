78defbf8361b19120cbb69fbd2e095aed7691b6a
Daniel <thedann8@gmail.com>
2021-09-08 11:44:22 +0200

    mob

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import "./App.css";
-import React, { useState } from "react";
+import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState('');
   const [movieList, setMovieList] = useState([]);
-  const API_URL = "http://www.omdbapi.com/?apikey=65049324&s="; // s=star+wars
+  const [selectedMovie, setSelectedMovie] = useState();
+
+  const API_URL = 'http://www.omdbapi.com/?apikey=65049324&s='; // s=star+wars
 
   const handleSearch = async (e) => {
     console.log(inputText);
+
+    setMovieList([]);
+
     const data = await fetch(`${API_URL}${inputText}`);
     const response = await data.json();
 
     if (data?.ok && response?.Response === 'True') {
-        setMovieList(response.Search);
+      setMovieList(response.Search);
+      console.log('response search', response.Search);
+      console.log('movieList movieList', movieList);
     }
-
   };
-    console.log(movieList);
   return (
     <div>
       <input
@@ -24,15 +29,21 @@ function App() {
           setInputText(e.target.value);
         }}
         value={inputText}
-        type="text"
-        placeholder="movie title"
+        type='text'
+        placeholder='movie title'
       />
       <button onClick={handleSearch}>Sök</button>
       <div>
-          {movieList.map(movie => (
-              <p key={movie.imdbID}>{movie.Title}</p>
-          ))}
+        {movieList.map((movie) => (
+          <p onClick={} key={movie.imdbID}>{movie.Title}</p>
+        ))}
       </div>
+      {selectedMovie && (
+        <div>
+          <p>test</p>
+          <p>test</p>
+        </div>
+      )}
     </div>
   );
 }
